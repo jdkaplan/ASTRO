8e66fb3220755030394ac11be3bc00c1a6120fec
rgomes <rgomes@mit.edu>
2013-06-15 16:25:06 -0400

    Added sleep mode for the serial driver, interrupt driven motors.

--- timer.c
+++ timer.c
@@ -5,11 +5,19 @@
 long int timerMS;
 
 void timerStart() {
-  P1DIR = 0x3;
-  P1OUT = 0x1;
   timerMS = 0;
   TACCR0 = TICKS_PER_MS;
-  TACTL = TASSEL_2 + MC_1 + TAIE + ID_2;
+  TAR = 0;
+  TACTL = TASSEL_2 + MC_1 + TAIE + ID_3;
+}
+
+void (*executable)();
+
+void executeAfterMS(int msTime, void (*fPtr)()) {
+  executable = fPtr;
+  TBCCR0 = msTime*TICKS_PER_MS;
+  TBR = 0;
+  TBCTL = TBSSEL_2 + MC_1 + TAIE + ID_3;
 }
 
 #pragma vector=TIMERA1_VECTOR
@@ -20,6 +28,18 @@ __interrupt void Timer_A(void) {
     case 10:
       TAR = 0;
       ++timerMS;
+      break;
+    }
+}
+
+#pragma vector=TIMERB1_VECTOR
+__interrupt void Timer_B(void) {
+  switch(TAIV) { // Efficient switch-implementation
+    case  2: break; // TACCR1 not used
+    case  4: break; // TACCR2 not used
+    case 10:
+      TBCTL = 0;
+      (*executable)();
       break;
     }
 }
