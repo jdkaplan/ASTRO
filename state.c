1719466c488962d111c18ef8011ec5a2411b2ed0
Rodrigo T. Gomes <rgomes@csail.mit.edu>
2013-07-18 22:06:25 -0400

    Started the state functions

--- state.c
+++ state.c
@@ -1,2 +1,34 @@
 #include <msp430.h>
 #include "state.h"
+
+volatile stateVec globalState;
+
+void saveState() {
+  
+}
+
+void retrieveState() {
+  
+}
+
+long generateChecksum() {
+  return
+    globalState.currentStepOne^
+    globalState.desiredStepOne^
+    globalState.prevSignalOne^
+
+    globalState.currentStepTwo^
+    globalState.desiredStepTwo^
+    globalState.prevSignalTwo^
+
+    globalState.internalTime^
+    globalState.externalTime^
+
+    globalState.temperature^
+
+    globalState.safemode;
+}
+
+char checkChecksum(long checksum) {
+  return (generateChecksum() == checksum);
+}
